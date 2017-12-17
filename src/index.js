da14e6c883b063b3786b9412818859f6f5f23d13
gowtthi <gowtthi@gmail.com>
2017-12-17 11:57:14 +0530

    LOL

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
+
+let name='gowtham'
+let gow={
+	fname:'gowthaman',
+	lname:'jayabalakrishnan'
+}
+const element=<h1>hey hi!!!</h1>:
+
 ReactDom.render(
-	<h1>Welcome to bit</h1>,
+	<div><h1>Welcome {name}{element}{gow.fname}{gow.lname} 
+	{element}</h1></div>,
+	
 	document.getElementById('root')
 	);

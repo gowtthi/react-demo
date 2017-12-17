c2bebefcf6658cf0ce089e68673848d62b47353f
gowtthi <gowtthi@gmail.com>
2017-12-17 22:30:07 +0530

    funtion

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-
-let name='gowtham'
-let gow={
-	fname:'gowthaman',
-	lname:'jayabalakrishnan'
+function gow(name='gowthaman'){
+	return <div><h1>hello{name}</h1></div>
 }
-const element=<h1>hey hi!!!</h1>:
+
 
 ReactDom.render(
-	<div><h1>Welcome {name}{element}{gow.fname}{gow.lname} 
-	{element}</h1></div>,
-	
+	gow(),
+
+
 	document.getElementById('root')
 	);

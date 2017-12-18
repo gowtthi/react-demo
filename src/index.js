63252cfc8463e84de8c2f8c7fd4bf61e36a5ca5b
gowtthi <gowtthi@gmail.com>
2017-12-18 22:40:42 +0530

    clock app :wq
    iwqiqqqqqmessage for your changes. Lines starting
    :wq
    
    quit<enter>
    quit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-function gow(name='gowthaman'){
-	return <div><h1>hello{name}</h1></div>
-}
-
+setInterval(function(){
 
 ReactDom.render(
-	gow(),
 
 
+ <h2>Time now: {new Date().toLocaleTimeString()}</h2>,
 	document.getElementById('root')
 	);
+
+
+
+},1000);
